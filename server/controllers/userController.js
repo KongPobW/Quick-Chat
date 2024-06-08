67e63c4e4f0ccf887c019717fd9bcc8c4cd9a7b9
KongPobW <KongPob.Wisitsak@gmail.com>
2024-06-08 13:31:30 +0700

    rewrite code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,21 +3,30 @@ const bcrypt = require("bcrypt");
 
 module.exports.signup = async (req, res, next) => {
     try {
+
         const { username, email, password } = req.body;
+
         const foundUsername = await User.findOne({ username });
-        if (foundUsername)
+        if (foundUsername) {
             return res.json({ msg: "username has already used", status: false });
+        }
+
         const foundEmail = await User.findOne({ email });
-        if (foundEmail)
+        if (foundEmail) {
             return res.json({ msg: "email has already used", status: false });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
+
         const user = await User.create({
             username,
             email,
             password: hashedPassword,
         });
+
         delete user.password;
         return res.json({ status: true, user });
+
     } catch (e) {
         next(e);
     }
@@ -25,15 +34,22 @@ module.exports.signup = async (req, res, next) => {
 
 module.exports.signin = async (req, res, next) => {
     try {
+
         const { username, password } = req.body;
+
         const user = await User.findOne({ username });
-        if (!user)
+        if (!user) {
             return res.json({ msg: "incorrect username", status: false });
+        }
+
         const isCorrectPassword = await bcrypt.compare(password, user.password);
-        if (!isCorrectPassword)
+        if (!isCorrectPassword) {
             return res.json({ msg: "incorrect password", status: false });
+        }
+
         delete user.password;
         return res.json({ status: true, user });
+
     } catch (e) {
         next(e);
     }
