bef4acdce5fa96cbbba61f3bf715d8223e62e516
KongPobW <KongPob.Wisitsak@gmail.com>
2024-06-20 21:05:26 +0700

    make chat input

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ChatInput from "./ChatInput";
 import Logout from "./Logout";
 
 function ChatContainer({ currentChat }) {
+
+    const handleSendMsg = async (msg) => {
+    };
+
     return (
         <div className="grid grid-rows-[10%,80%,10%] gap-[0.1rem] overflow-hidden custom-range:grid-rows-[15%,70%,15%]">
             <div className="flex justify-between items-center px-8">
@@ -13,6 +18,8 @@ function ChatContainer({ currentChat }) {
                 </div>
                 <Logout />
             </div>
+            <div></div>
+            <ChatInput handleSendMsg={handleSendMsg} />
         </div>
     );
 }
---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { IoMdSend } from "react-icons/io";
+
+function ChatInput({ handleSendMsg }) {
+
+    const [message, setMessage] = useState("");
+
+    const handleSend = (event) => {
+        event.preventDefault();
+        if (message.length > 0) {
+            handleSendMsg(message);
+            setMessage("");
+        }
+    };
+
+    return (
+        <div className="grid items-center grid-cols-[95%,5%] bg-[#080420] px-8 custom-range:px-4">
+            <form className="w-full rounded-2xl flex items-center gap-8 bg-white/20" onSubmit={(event) => handleSend(event)}>
+                <input
+                    className="w-[100%] h-[60%] bg-transparent text-white border-none pl-4 text-[1.2rem] focus:outline-none"
+                    type="text"
+                    placeholder="type your message here..."
+                    onChange={(e) => setMessage(e.target.value)}
+                    value={message}
+                />
+                <button className="p-3.5 flex items-center justify-center bg-purple-600 custom-range:px-3 custom-range:py-1" type="submit">
+                    <IoMdSend className="text-white text-2xl custom-range:text-base" />
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default ChatInput;
